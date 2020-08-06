1d12934d80ba58192f172d2b7bdeb457e39ee8af
Felipe Cecatto <60164757+cecattoNinetyNine@users.noreply.github.com>
2020-08-06 18:35:28 -0300

    Create      Problem_2235_Walking_in_Time.c

---
+++ Problem_2235_Walking_in_Time.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int A, B, C;
+    scanf("%d %d %d", &A, &B, &C);
+    if(A + B == C || C + A == B ||  C + B == A) printf("S\n");
+    else if(A - B == 0 || A - C == 0 || B - C == 0) printf("S\n");
+    else printf("N\n");
+    return 0;
+}
